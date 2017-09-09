fbd30ebad5ec73817bdd6fc69332ccb1fd758ff8
David <david.healey@gmail.com>
2017-09-08 22:56:06 -0600

    fix train_test_split arg ordering

--- getting_data/train_test_split.sh
+++ getting_data/train_test_split.sh
@@ -3,11 +3,14 @@ FILE=$1
 TEST_FRACTION=$2
 TARGET_DIRECTORY=$3
 
+echo $FILE
+echo $TARGET_DIRECTORY
+
 mkdir -p $TARGET_DIRECTORY
 
 N=`cat $FILE | wc -c`
 TEST_N=`python -c "print(int($TEST_FRACTION * $N))"`
 TRAIN_N=$(($N-$TEST_N))
 
-head $FILE -c $TRAIN_N > $TARGET_DIRECTORY/train
-tail $FILE -c $TEST_N > $TARGET_DIRECTORY/test
+head -c $TRAIN_N $FILE  > $TARGET_DIRECTORY/train
+tail -c $TEST_N $FILE > $TARGET_DIRECTORY/test
